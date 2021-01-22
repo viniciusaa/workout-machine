f0eb0b77a33c5be788d00f1446b1d99c5c1ec0fc
“Vinícius <vini89go@gmail.com>
2021-01-22 03:35:14 -0300

    Updated exercise model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,8 @@
 class Exercise < ApplicationRecord
   validates :name, presence: true
-  validates :amount, presence: true
+  validates :sets, numericality: { only_integer: true }, allow_blank: true
+  validates :duration, numericality: { only_integer: true }, allow_blank: true
+  validates :effort_level, numericality: {
+    only_integer: true, greater_than_: 0, less_than: 6
+  }, allow_blank: true
 end

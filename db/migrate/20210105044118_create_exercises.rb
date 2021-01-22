8cae26c226f778a0d041f463347a8541a3083640
“Vinícius <vini89go@gmail.com>
2021-01-22 03:12:09 -0300

    Updated exercises in schema

--- db/migrate/20210105044118_create_exercises.rb
+++ db/migrate/20210105044118_create_exercises.rb
@@ -2,7 +2,9 @@ class CreateExercises < ActiveRecord::Migration[6.1]
   def change
     create_table :exercises do |t|
       t.string :name
-      t.integer :amount
+      t.integer :sets
+      t.integer :duration
+      t.integer :effort_level
       t.timestamps
     end
   end

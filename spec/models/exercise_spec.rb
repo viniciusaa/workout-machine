836f8119c26134887b299bd3ec6a46c7e09bf210
“Vinícius <vini89go@gmail.com>
2021-01-25 17:13:17 -0300

    Updated exercise model tests

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -7,9 +7,45 @@ RSpec.describe Exercise, type: :model do
     end
   end
 
-  describe "amount" do
-    it "should be valid" do
-      should validate_presence_of(:amount)
+  describe "sets" do
+    it "should be integer" do
+      should validate_numericality_of(:sets)
+    end
+
+    it "can be present" do
+      should allow_value(1).for(:sets)
+    end
+
+    it "can be blank" do
+      should allow_value("", nil).for(:sets)
+    end
+  end
+
+  describe "duration" do
+    it "should be integer" do
+      should validate_numericality_of(:duration)
+    end
+
+    it "can be present" do
+      should allow_value(1).for(:duration)
+    end
+
+    it "can be blank" do
+      should allow_value("", nil).for(:duration)
+    end
+  end
+
+  describe "effort_level" do
+    it "should be integer" do
+      should validate_numericality_of(:effort_level)
+    end
+
+    it "can be present" do
+      should allow_value(1).for(:effort_level)
+    end
+
+    it "can be blank" do
+      should allow_value("", nil).for(:effort_level)
     end
   end
 end

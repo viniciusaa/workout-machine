9b9f299e95e918ef5a88e1cb44da24b96a65d2c7
“Vinícius <vini89go@gmail.com>
2021-01-26 01:48:40 -0300

    Add workout model and association

---
+++ db/migrate/20210126043914_create_join_table_exercise_workout.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableExerciseWorkout < ActiveRecord::Migration[6.1]
+  def change
+    create_join_table :exercises, :workouts do |t|
+      # t.index [:exercise_id, :workout_id]
+      # t.index [:workout_id, :exercise_id]
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_041546) do
+ActiveRecord::Schema.define(version: 2021_01_26_043914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 2021_01_26_041546) do
     t.integer "effort_level"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "exercises_workouts", id: false, force: :cascade do |t|
+    t.bigint "exercise_id", null: false
+    t.bigint "workout_id", null: false
   end
 
   create_table "workouts", force: :cascade do |t|

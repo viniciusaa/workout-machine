9eec71e9c6d97ea32e0cc46a5985209f7fafd72c
“Vinícius <vini89go@gmail.com>
2021-01-26 01:54:59 -0300

    Updated exercise model spec

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -2,50 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Exercise, type: :model do
   describe "name" do
-    it "should be valid" do
-      should validate_presence_of(:name)
-    end
+    it { should validate_presence_of(:name) }
   end
 
   describe "sets" do
-    it "should be integer" do
-      should validate_numericality_of(:sets)
-    end
-
-    it "can be present" do
-      should allow_value(1).for(:sets)
-    end
-
-    it "can be blank" do
-      should allow_value("", nil).for(:sets)
-    end
+    it { should validate_numericality_of(:sets) }
+    it { should allow_value(1).for(:sets) }
+    it { should allow_value("", nil).for(:sets) }
   end
 
   describe "duration" do
-    it "should be integer" do
-      should validate_numericality_of(:duration)
-    end
-
-    it "can be present" do
-      should allow_value(1).for(:duration)
-    end
-
-    it "can be blank" do
-      should allow_value("", nil).for(:duration)
-    end
+    it { should validate_numericality_of(:duration) }
+    it { should allow_value(1).for(:duration) }
+    it { should allow_value("", nil).for(:duration) }
   end
 
   describe "effort_level" do
-    it "should be integer" do
-      should validate_numericality_of(:effort_level)
-    end
-
-    it "can be present" do
-      should allow_value(1).for(:effort_level)
-    end
-
-    it "can be blank" do
-      should allow_value("", nil).for(:effort_level)
-    end
+    it { should validate_numericality_of(:effort_level) }
+    it { should allow_value(1).for(:effort_level) }
+    it { should allow_value("", nil).for(:effort_level) }
   end
+
+  it { should have_and_belong_to_many(:workouts) }
 end

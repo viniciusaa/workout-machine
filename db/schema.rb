30923399878e200f99cda43802be9fcd1d5fcc7e
“Vinícius <vini89go@gmail.com>
2021-01-26 01:20:26 -0300

    Added workouts do schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_044118) do
+ActiveRecord::Schema.define(version: 2021_01_26_041546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2021_01_05_044118) do
     t.integer "sets"
     t.integer "duration"
     t.integer "effort_level"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

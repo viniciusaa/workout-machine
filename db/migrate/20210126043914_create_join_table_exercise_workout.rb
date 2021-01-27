0f02c965a566ab975aa88c97f9fe90725082f7fc
“Vinícius <vini89go@gmail.com>
2021-01-27 01:00:46 -0300

    Updated join table, removing comments

--- db/migrate/20210126043914_create_join_table_exercise_workout.rb
+++ db/migrate/20210126043914_create_join_table_exercise_workout.rb
@@ -2,8 +2,8 @@
 class CreateJoinTableExerciseWorkout < ActiveRecord::Migration[6.1]
   def change
     create_join_table :exercises, :workouts do |t|
-      # t.index [:exercise_id, :workout_id]
-      # t.index [:workout_id, :exercise_id]
+      t.index [:exercise_id, :workout_id]
+      t.index [:workout_id, :exercise_id]
     end
   end
 end

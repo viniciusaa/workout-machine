8efe0c2a8dcd3282fa81e8e17a22fc1030608214
“Vinícius <vini89go@gmail.com>
2021-01-27 03:20:59 -0300

    Added new and create actions to workout

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,19 @@
 # Will be able to store a list of exercises
 class WorkoutsController < ApplicationController
-  def show; end
+  def show
+    @workout = Workout.find(params[:id])
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new
+    if @workout.save
+      redirect_to @workout
+    else
+      render :new
+    end
+  end
 end

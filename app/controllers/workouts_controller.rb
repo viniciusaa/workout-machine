8f39ffbe05bb98589b654e33b865a40cf35b965b
“Vinícius <vini89go@gmail.com>
2021-01-29 02:13:01 -0300

    Refactored workouts controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -8,7 +8,7 @@ class WorkoutsController < ApplicationController
     Workout.all.find { |workout| @workout = workout if workout.created_at.to_date == DateTime.now.to_date }
     @workout ||= Workout.create
 
-    Exercise.all.sample(6).select { |exercise| @workout.exercises << exercise } if @workout.exercises.empty?
+    @workout.exercises << Exercise.all.sample(6) if @workout.exercises.empty?
 
     @latest_workouts = Workout.order(created_at: :desc).first(7)
   end

d29b8f70df770b5b20f8a5b560e13e6a9d195ea8
“Vinícius <vini89go@gmail.com>
2021-02-02 02:37:23 -0300

    Added exercise and updated workout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :workouts
+  resources :workouts, only: [:show, :new]
+  resources :exercises, only: [:show, :new, :edit]
 end

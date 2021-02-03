c3aeb540858489d415063292da4b2ee600009bbc
“Vinícius <vini89go@gmail.com>
2021-02-03 02:31:51 -0300

    Add list all exercises spec

---
+++ spec/features/exercises/list_all_exercises_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.feature "List all exercises", type: :feature do
+  scenario do
+    @first_exercise = create(:exercise)
+    @second_exercise = create(:exercise)
+    @third_exercise = create(:exercise)
+    visit new_workout_path
+    click_link "Check all exercises"
+    expect(page).to have_content("Name: #{@first_exercise.name}")
+    expect(page).to have_content("Name: #{@second_exercise.name}")
+    expect(page).to have_content("Name: #{@third_exercise.name}")
+  end
+end

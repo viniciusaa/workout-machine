87778210bf383eb10179ea9510fc511a89012cd4
“Vinícius <vini89go@gmail.com>
2021-02-03 02:29:31 -0300

    Add display exercise spec

---
+++ spec/features/exercises/display_exercise_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.feature "Display exercise", type: :feature do
+  scenario do
+    @exercise = create(:exercise)
+    visit new_workout_path
+    click_link @exercise.name
+    expect(page).to have_content("Name: #{@exercise.name}")
+    expect(page).to have_content("Sets: #{@exercise.sets}")
+    expect(page).to have_content("Duration: #{@exercise.duration} min")
+    expect(page).to have_content("Difficulty: #{@exercise.effort_level}")
+  end
+end

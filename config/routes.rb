0f3e96321741fee62bfc8149eb1576ddad13dd5b
“Vinícius <vini89go@gmail.com>
2021-02-04 00:58:15 -0300

    Set root to workouts/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :workouts, only: [:show, :new]
+  root "workouts#new"
   resources :exercises
 end

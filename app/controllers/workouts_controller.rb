a8e3ac6fabec5ccec4e012f5f8f1193065471849
“Vinícius <vini89go@gmail.com>
2021-02-04 12:37:08 -0300

    Added conditional to the Workout controller to handle with the Daily Workout Remake

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -6,6 +6,12 @@ class WorkoutsController < ApplicationController
 
   def new
     Workout.all.find { |workout| @workout = workout if workout.created_at.to_date == DateTime.now.to_date }
+
+    if params[:remake]
+      @workout.exercises.clear
+      redirect_to new_workout_path
+    end
+
     @workout ||= Workout.create
 
     @workout.exercises << Exercise.all.sample(6) if @workout.exercises.empty?

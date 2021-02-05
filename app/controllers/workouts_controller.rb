2a1c861e183c4f4fa0005a4a47abb220cec9e5b3
“Vinícius <vini89go@gmail.com>
2021-02-05 02:05:35 -0300

    Workout Create action now handle the workout creation and addition of exercises to it

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -6,12 +6,13 @@ class WorkoutsController < ApplicationController
 
   def new
     Workout.all.find { |workout| @workout = workout if workout.created_at.to_date == DateTime.now.to_date }
-    @workout ||= Workout.create
-
-    @workout.exercises << Exercise.all.sample(6) if @workout.exercises.empty?
+    @workout.nil?? self.create
 
     @latest_workouts = Workout.order(created_at: :desc).first(7)
   end
 
-  def create; end
+  def create
+    @workout = Workout.create
+    @workout.exercises << Exercise.all.sample(6)
+  end
 end

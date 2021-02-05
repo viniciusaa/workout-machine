5b666668f4b26d3b720756dbcb8714af139b75da
“Vinícius <vini89go@gmail.com>
2021-02-05 02:10:33 -0300

    Removed New View, daily workout now is displayed on respective Show View

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,17 +2,17 @@
 class WorkoutsController < ApplicationController
   def show
     @workout = Workout.find(params[:id])
+    @latest_workouts = Workout.order(created_at: :desc).first(7)
   end
 
   def new
-    Workout.all.find { |workout| @workout = workout if workout.created_at.to_date == DateTime.now.to_date }
-    @workout.nil?? self.create
-
-    @latest_workouts = Workout.order(created_at: :desc).first(7)
+    Workout.all.find { |workout| @workout = workout if DailyWorkout.new(workout).todays_workout? }
+    @workout.nil?? self.create : (redirect_to workout_path(@workout))
   end
 
   def create
     @workout = Workout.create
     @workout.exercises << Exercise.all.sample(6)
+    redirect_to workout_path(@workout)
   end
 end

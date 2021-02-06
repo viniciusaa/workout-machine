008e4afb89aed70ce9848dbfb3cbfa3d208ec410
“Vinícius <vini89go@gmail.com>
2021-02-06 03:19:59 -0300

    Swapped New action and route for Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :workouts, only: [:show, :new, :create, :destroy]
-  root "workouts#new"
+  resources :workouts, only: [:show, :index, :create, :destroy]
+  root "workouts#index"
   resources :exercises
 end

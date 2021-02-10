c1cf5edc94e6da3765b3ba3350d9b5bbb69edb77
“Vinícius <vini89go@gmail.com>
2021-02-10 02:16:49 -0300

    Removed set workout private method from workouts controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,23 +1,16 @@
 class WorkoutsController < ApplicationController
-  before_action :set_workout, only: [:show, :update]
-
   def show
+    @workout = Workout.find(params[:id])
     @latest_workouts = Workout.order(created_at: :desc).first(7)
   end
 
   def index
     Workout.all.find { |workout| @workout = workout if workout.todays_workout? }
-    
+
     CreateWorkoutService.create if @workout.nil?
 
     RemakeWorkoutService.new(@workout).remake if params[:remake]
 
     redirect_to workout_path(Workout.last)
-  end
-
-  private
-
-  def set_workout
-    @workout = Workout.find(params[:id])
   end
 end

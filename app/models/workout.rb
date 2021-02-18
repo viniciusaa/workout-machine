34f1a74535a56920551eef67461c514913e7f5a5
“Vinícius <vini89go@gmail.com>
2021-02-18 01:34:41 -0300

    Updated Workout model to follow rubocop best practices

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,7 +1,7 @@
 class Workout < ApplicationRecord
   has_and_belongs_to_many :exercises
 
-  scope :created_today, ->() { where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_today, -> { where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
 
   def todays_workout?
     created_at.to_date == DateTime.now.to_date
